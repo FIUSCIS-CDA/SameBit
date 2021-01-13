293e35ac04902fa6d88bea1797470bd8744f0451
tcickovs <tcickovs@fiu.edu>
2021-01-13 15:49:40 -0500

    Fixed a bug begin5 -> begin

--- testbench.v
+++ testbench.v
@@ -8,7 +8,7 @@ SameBit mySame(A, S);
 
 initial begin
 A=0;   #10; 
-if (S !== A) begin5
+if (S !== A) begin
            $display("Error with A=%d: Should also be %d but got %d", A, A, S); $stop;
 end
 A=1;   #10; 
