75c93776f1d1db347ca33ddfa57790a505c198b5
tcickovs <tcickovs@fiu.edu>
2021-02-04 14:43:31 -0500

    Verilog

---
+++ SameBit.v
@@ -0,0 +1,35 @@
+// Copyright (C) 1991-2015 Altera Corporation. All rights reserved.
+// Your use of Altera Corporation's design tools, logic functions 
+// and other software and tools, and its AMPP partner logic 
+// functions, and any output files from any of the foregoing 
+// (including device programming or simulation files), and any 
+// associated documentation or information are expressly subject 
+// to the terms and conditions of the Altera Program License 
+// Subscription Agreement, the Altera Quartus II License Agreement,
+// the Altera MegaCore Function License Agreement, or other 
+// applicable license agreement, including, without limitation, 
+// that your use is for the sole purpose of programming logic 
+// devices manufactured by Altera and sold by Altera or its 
+// authorized distributors.  Please refer to the applicable 
+// agreement for further details.
+
+// PROGRAM		"Quartus II 64-Bit"
+// VERSION		"Version 15.0.0 Build 145 04/22/2015 SJ Web Edition"
+// CREATED		"Mon Feb  1 19:57:18 2021"
+
+module SameBit(
+	Ain,
+	Aout
+);
+
+
+input wire	Ain;
+output wire	Aout;
+
+
+assign	Aout = Ain;
+
+
+
+
+endmodule
