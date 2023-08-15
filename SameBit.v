cf0253c20c91d7fa1280a0a5ece12384d09d0c35
Trevor Cickovski <movingpictures83@gmail.com>
2023-08-15 10:13:19 -0400

    Upgraded Quartus

--- SameBit.v
+++ SameBit.v
@@ -15,7 +15,7 @@
 
 // PROGRAM		"Quartus Prime"
 // VERSION		"Version 20.1.1 Build 720 11/11/2020 SJ Lite Edition"
-// CREATED		"Tue May 10 09:19:23 2022"
+// CREATED		"Tue Aug 15 10:11:01 2023"
 
 module SameBit(
 	Ain,
